7f70a637c6bd4cec1d2ced3423736c121e341ec0
woodinnis <wood.innis@gmail.com>
2015-06-24 18:30:39 -0700

    Pickup added
    A basic pickup has been added for testing purposes. A pickup tag has
    also been added.
    Collision checking for the pickup tag has been added.
    Y-axis camera control has been added.
    
    Renamed playerNavigation to PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,42 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class playerNavigation : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	public float speed = 6.0f;
 	public float jumpSpeed = 4.0f;
 	public float gravity = 20.0f;
 
-	public float rotation = 220.0f;
+	private Vector3 rotation = Vector3.zero;
 
 	private Vector3 moveDirection = Vector3.zero;
 
 	void Awake()
 	{
 		DontDestroyOnLoad(transform.gameObject);
-
-		//if(Application.isLoadingLevel)
-		//	transform.position = Vector3.zero;
 	}
+
 	// Update is called once per frame
 	void Update () {
 		CharacterController controller = GetComponent<CharacterController> ();
 
 		// Basic Movement
-		moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-		moveDirection = transform.TransformDirection (moveDirection);
+		float moveHoriz = Input.GetAxis ("Horizontal");
+		float moveVert = Input.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3(moveHoriz, 0 , moveVert);
+
+		moveDirection = transform.TransformDirection (move);
 		moveDirection *= speed;
 
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 
-		rotation += Input.GetAxis ("Mouse X");
-		transform.eulerAngles = new Vector3 (0, rotation, 0);
+		// Rotate player view to follow the mouse
+		rotation.x += Input.GetAxis ("Mouse X");
+		rotation.y -= Input.GetAxis ("Mouse Y");
+		transform.eulerAngles = new Vector3(rotation.y,rotation.x,0);
 
 		// If the player clicks on the door
 		if(Input.GetMouseButtonDown(0))
 		{
 			doorEnter();
+		}
+	}
+
+	// Deactivate any elements tagged as pickups when they are collided with
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pickup")) 
+		{
+			other.gameObject.SetActive(false);
 		}
 	}
 
