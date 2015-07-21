6dcc1a408dbb42d3b9fdcc4b448a25ee422ebc17
woodinnis <wood.innis@gmail.com>
2015-07-21 11:56:43 -0700

    Old Man
    Old Man Dialogue added, functional
    DialogueController created, not built

--- Assets/Scripts/OldManController.cs
+++ Assets/Scripts/OldManController.cs
@@ -1,16 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class OldManController : MonoBehaviour {
 
 	public float AdjustY;
+	public TextAsset DialogueFile;
+	public float timeToWait;
+
 	private GameController game;
+	private string[] Dialogue;
+	private int dialogueLength;
+	private Text text;
 
 	// Use this for initialization
 	void Start () {
 		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
 		game = gameControllerObject.GetComponent<GameController>();
 
+		GameObject textController = GameObject.FindWithTag("UI");
+		text = textController.GetComponent<Text>();
+
+		Dialogue = DialogueFile.text.Split ('\n');
+		dialogueLength = Dialogue.Length;
 	}
 	
 	// Update is called once per frame
@@ -21,9 +33,25 @@ public class OldManController : MonoBehaviour {
 	}
 
 	void OnMouseEnter(){
-		print ("HI SONNY!");
+		//for(int i = 0; i < dialogueLength; i++)
+		//{
+			StartCoroutine(Speek());
+			//text.text = Dialogue[i];
+			//YieldInstruction WaitForSeconds(timeToWait);
+		//}
 	}
 	void OnMouseExit(){
-		game.oldManTalk = true;	
+		//game.oldManTalk = true;	
+	}
+
+	IEnumerator Speek()
+	{
+		//while (true) {
+		for(int i = 0; i < dialogueLength; i++)
+		{
+			//Debug.Log ("Green");
+			text.text = Dialogue[i];
+			yield return new WaitForSeconds(timeToWait);
+		}
 	}
 }
